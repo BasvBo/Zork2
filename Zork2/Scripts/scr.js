1de7a8d2891750e5d33fda34d628e314fb6d1dbb
Agroeningen <37032183+Agroeningen@users.noreply.github.com>
2018-06-27 19:44:04 +0200

    frontend deel
    niet af

---
+++ Zork2/Scripts/scr.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    four();
+    $('#sendBtn').click(function () {
+        $('.textHolder').animate({ scrollTop: $('.textHolder').prop("scrollHeight") }, 1000);
+        
+        document.getElementById("inputField").focus();
+    })
+})
+    var elements = document.getElementsByClassName("column");
+    var i;
+
+function four() {
+
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "25%";
+    }
+}
